f7e398c13ed1a5704adca914262b51c8d1e0a50a
proyectoxultra-boop <proyectoxultra@gmail.com>
2025-11-02 01:06:08 -0600

    Delete newsFetcher.js

--- newsFetcher.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-
-export async function getCryptoTechNews() {
-  const q = encodeURIComponent("cryptocurrency OR crypto OR blockchain OR technology");
-  const url = `https://newsapi.org/v2/everything?q=${q}&language=en&pageSize=3&apiKey=${process.env.NEWSAPI_KEY}`;
-  const r = await fetch(url);
-  const json = await r.json();
-  return (json.articles || []).filter(a => a.title && (a.description || a.content)).slice(0,3);
-}
